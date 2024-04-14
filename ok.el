83b328d995f77643f1bfb638b329a91464399cac
Taro Sato <okomestudio@gmail.com>
2024-04-14 01:57:40 -0700

    Add ok--repend-timestamp and advice

--- ok.el
+++ ok.el
@@ -15,9 +15,24 @@
 
 (defun ok-debug-message (text)
   "Print a debug message TEXT with timestamp."
-  (message "%s: %s"
+  (message "%s %s"
            (format-time-string "%FT%H:%M:%S.%3N" (current-time))
            text))
+
+(defun ok-prepend-timestamp (format-string &rest args)
+  "Prepend timestamp to `message' output.
+FORMAT-STRING and ARGS are passed through. See
+https://emacs.stackexchange.com/a/38511/599."
+  (unless (string-equal format-string "%s%s")
+    (let ((deactivate-mark nil)
+          (inhibit-read-only t))
+      (with-current-buffer "*Messages*"
+        (goto-char (point-max))
+        (if (not (bolp))
+            (newline))
+        (insert (format-time-string "%FT%H:%M:%S.%3N" (current-time)) " ")))))
+
+(advice-add 'message :before 'ok-prepend-timestamp)
 
 (defun ok-execution-time (name &rest body)
   "Measure execution time of BODY named NAME."
