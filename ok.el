9ddae79ca86885f7d068acf3af4563bdfab4310c
Taro Sato <okomestudio@gmail.com>
2025-08-03 16:29:08 -0700

    Implement ok-string-text-to-slug

--- ok.el
+++ ok.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Taro Sato <okomestudio@gmail.com>
 ;; URL: https://github.com/okomestudio/ok.el
-;; Version: 0.10.4
+;; Version: 0.11.1
 ;; Keywords: development, convenience
 ;; Package-Requires: ((emacs "29.1") (dash "2.19.1"))
 ;;
